8d602f53af48cef1dc7e5f29d041adb98ba948c2
juangallino <juangallino>
2021-12-20 13:44:07 -0300

    Update

---
+++ src/components/Pedido/materialesCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MaterialesCard = () => {
+    return (
+        <div>
+            <h1>hola gil</h1>
+            <i class="fas fa-h2"> hola este es la card</i>
+        </div>
+    )
+}
+
+export default MaterialesCard
